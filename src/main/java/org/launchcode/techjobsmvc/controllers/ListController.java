06d7792c4608cae250e99e5d9a48d1b9943f7c2c
Hector Trillo <htrillo95@gmail.com>
2024-05-31 08:46:53 -0400

    update 15 of 17

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -28,6 +28,8 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+        //TASK #2 - Add View All Link Below
+
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +28,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    //TASK 3 BELOW
+    @PostMapping(value = "results") //handles post request
+    public String displaySearchResults(Model model, @RequestParam String searchType, //param 1
+                                                    @RequestParam String searchTerm) //param 2
+    {
+        ArrayList<Job> jobs;
+        if (searchType.equals("all") || searchTerm.isEmpty()) { //lists all or left empty depending on input
+            jobs = JobData.findAll(); //based on search type/term
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm); //else list based on search type/term
+        }
+        model.addAttribute("jobs", jobs); //adds to model
+        model.addAttribute("columns", columnChoices); //adds to model
+        return "search"; //returns display search to be passed into view
+    }
 
 }
 
